29185ea0589c60342352d5973045efd7f21bc085
Reon-1 <reon.bkt@gmail.com>
2026-08-20 22:19:53 +0545

    refactored

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@ fn main() {
     println!("2. Exit");
 
     print!("Select One Option: ");
-
     io::stdout().flush().expect("Failed to flush stdout");
 
     let mut input = String::new();
@@ -24,40 +23,42 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read line");
 
-    let trimmed_input = input.trim();
+    let user_input = input.trim();
 
-    println!("You chose option {}", trimmed_input);
+    println!("You chose option {}", user_input);
     println!("-----------------------------");
 
-    // One Attack
+    // Create first attack
     let first_attack = Attack {
         source_country: String::from("China"),
         target_country: String::from("USA"),
         attack_type: String::from("DDoS"),
     };
 
+    // Create second attack
     let second_attack = Attack {
         source_country: String::from("Russia"),
         target_country: String::from("Germany"),
         attack_type: String::from("Malware"),
     };
 
-    // list of Attacks
+    // Create empty list
     let mut attacks: Vec<Attack> = Vec::new();
 
-    if trimmed_input == "1" {
-        attacks.push(first_attack);
-        println!("Source: {}", attacks[0].source_country);
-        println!("Target: {}", attacks[0].target_country);
-        println!("Type: {}", attacks[0].attack_type);
+    // Put both attacks into the list
+    attacks.push(first_attack);
+    attacks.push(second_attack);
 
-        println!("-----------------------------");
-
-        attacks.push(second_attack);
-        println!("Source: {}", attacks[1].source_country);
-        println!("Target: {}", attacks[1].target_country);
-        println!("Type: {}", attacks[1].attack_type);
-    } else if trimmed_input == "2" {
+    // Handle user choice
+    if user_input == "1" {
+        // Go through every attack in the list
+        for attack in attacks {
+            println!("Source: {}", attack.source_country);
+            println!("Target: {}", attack.target_country);
+            println!("Type: {}", attack.attack_type);
+            println!("-----------------------------");
+        }
+    } else if user_input == "2" {
         println!("Exit!");
     } else {
         println!("Invalid Choice!");
